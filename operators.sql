6f46e6cb97442e7b2d6e3faf5c8ef79737ae12ca
Carlo Sganzerla <carlosganzrl@hotmail.com>
2023-04-14 11:50:27 -0300

    Added last query

--- operators.sql
+++ operators.sql
@@ -1,11 +1,29 @@
--- Opclasses from an access method
-SELECT opcname, amopstrategy, amopopr::regoperator
-FROM pg_am am
-JOIN pg_opfamily opf ON opfmethod = am.oid
-JOIN pg_opclass opc ON opcfamily = opf.oid
-JOIN pg_amop amop ON amopfamily = opcfamily
-WHERE amname = 'btree'  AND opf.opfname = 'integer_ops'
-ORDER BY opcname, amopstrategy;
+-- Classes, access method and families from an access method
+SELECT
+    am.amname access_method,
+    opcname class_name,
+    opfname family_name,
+    amopopr::regoperator
+FROM 
+    pg_am am
+JOIN 
+    pg_opfamily opf 
+ON
+    opfmethod = am.oid
+JOIN
+    pg_opclass opc 
+ON
+    opcfamily = opf.oid
+JOIN
+    pg_amop amop
+ON
+    amopfamily = opcfamily
+WHERE
+    am.amname = 'btree'
+ORDER BY 
+    access_method,
+    family_name,
+    class_name;
 
 -- Access method properties
 SELECT
